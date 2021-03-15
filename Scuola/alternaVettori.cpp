12c0ce808968eafae883187c5b88ecb6db4f5608
Sansshrek <franci.pagliaccia13@gmail.com>
2021-03-15 23:18:10 +0100

    code indentation

--- Scuola/alternaVettori.cpp
+++ Scuola/alternaVettori.cpp
@@ -4,35 +4,29 @@ elementi). Si progetti una funzione ALTERNA che riceva in ingresso i due vettori
 e restituisca un vettore vett3 costituito alternando gli elementi di vett1 e
 vett2. La funzione ALTERNA deve anche restituire come valore di ritorno la somma
 di tutti gli elementi del vettore vett3.  */
+#include <iostream>
 #include <stdlib.h>
 #include <time.h>
-
-#include <iostream>
 using namespace std;
 int vett3[100], vett1[50], vett2[50];
 
-void Random(int max)
-{
+void Random(int max) {
     srand(time(0));
     cout << "\nVettore 1:" << endl;
-    for (int i = 0; i < max; i++)
-    {
+    for (int i = 0; i < max; i++) {
         vett1[i] = (rand() % 30);
         cout << vett1[i] << " ";
     }
     cout << "\nvettore 2:" << endl;
-    for (int i = 0; i < max; i++)
-    {
+    for (int i = 0; i < max; i++) {
         vett2[i] = (rand() % 30);
         cout << vett2[i] << " ";
     }
 }
 
-int Alterna(int max)
-{
+int Alterna(int max) {
     int j = 0;
-    for (int i = 0; i < max; i++)
-    {
+    for (int i = 0; i < max; i++) {
         vett3[j] = vett1[i];
         j++;
         vett3[j] = vett2[i];
@@ -40,8 +34,7 @@ int Alterna(int max)
     }
     int som = 0;
     cout << "\n\nVettore 3:" << endl;
-    for (int i = 0; i < max * 2; i++)
-    {
+    for (int i = 0; i < max * 2; i++) {
         som += vett3[i];
         cout << vett3[i] << " ";
     }
@@ -49,8 +42,7 @@ int Alterna(int max)
     return som;
 }
 
-int main()
-{
+int main() {
     int max;
     cout << "Inserire massima lunghezza dei vettori" << endl;
     cin >> max;
@@ -59,15 +51,12 @@ int main()
     som = Alterna(max);
     cout << "La somma dei valori di vett3 = " << som << endl;
     int scelta;
-    do
-    {
-        cout << "\nVuoi continuare?" << endl
-             << "si=1   no=0" << endl;
+    do {
+        cout << "\nVuoi continuare?" << endl << "si=1   no=0" << endl;
         cin >> scelta;
         if (scelta != 0 && scelta != 1)
             cout << "Reinserire valore scelta" << endl;
-        else if (scelta == 1)
-        {
+        else if (scelta == 1) {
             Random(max);
             som = Alterna(max);
             cout << "La somma dei valori di vett3 = " << som << endl;
